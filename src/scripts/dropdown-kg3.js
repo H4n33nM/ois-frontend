92b3f1049edef2e718ae9cf5e7186fe1dd955ebf
Haneen <haninmohamed@graduate.utm.my>
2025-01-22 23:07:37 +0300

    adjusted all kg menu pages

--- src/scripts/dropdown-kg3.js
+++ src/scripts/dropdown-kg3.js
@@ -1,9 +1,9 @@
-var dropdown = document.getElementsByClassName("kg-wr-button");
+var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active-w");
+    this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
